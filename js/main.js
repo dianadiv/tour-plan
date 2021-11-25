93b09f1b6d4f91e951de9ef754961d79dc4774f1
dianadiv <diana.gs202@gmail.com>
2021-11-25 11:21:54 +0200

    fixed responsive

--- js/main.js
+++ js/main.js
@@ -10,20 +10,6 @@ const swiper = new Swiper('.hotel-slider', {
     prevEl: '.hotel-slider__button--prev',
   },
 });
-// JavaScript API от Яндекс.Карт
-ymaps.ready(init);
-
-function init() {
-    var map = new ymaps.Map('map', {
-        center: [36.708957, 28.723924],
-        zoom: 8,
-    });
-
-    var Placemark = new ymaps.Placemark([36.708957, 28.723924], {
-    });
-    map.geoObjects.add(Placemark);
-}
-
 const reviewSlider = new Swiper('.reviews-slider', {
   // Optional parameters
   loop: true,
