8feb4fda907813aef2b0b881f57968cfe949b15d
dianadiv <diana.gs202@gmail.com>
2021-12-01 18:32:45 +0200

    create mobile menu

--- js/main.js
+++ js/main.js
@@ -22,3 +22,11 @@ const reviewSlider = new Swiper('.reviews-slider', {
     prevEl: '.reviews-slider__button--prev',
   },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function(){
+  console.log("Клик по кнопке меню");
+  document
+    .querySelector(".navbar-bottom").
+    classList.toggle("navbar-bottom--visible")
+});
