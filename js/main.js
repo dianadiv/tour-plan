02f96583d207011efd1bc9093d6c28656803c534
dianadiv <diana.gs202@gmail.com>
2021-12-03 20:45:33 +0200

    add modal dialog

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const swiper = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+  const swiper = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   keyboard: {
@@ -9,8 +10,8 @@ const swiper = new Swiper('.hotel-slider', {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
   },
-});
-const reviewSlider = new Swiper('.reviews-slider', {
+  });
+  const reviewSlider = new Swiper('.reviews-slider', {
   // Optional parameters
   loop: true,
   keyboard: {
@@ -21,12 +22,28 @@ const reviewSlider = new Swiper('.reviews-slider', {
     nextEl: '.reviews-slider__button--next',
     prevEl: '.reviews-slider__button--prev',
   },
-});
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function(){
-  console.log("Клик по кнопке меню");
-  document
-    .querySelector(".navbar-bottom").
-    classList.toggle("navbar-bottom--visible")
+  var menuButton = $(".menu-button");
+  menuButton.on('click', function(){
+   $(".navbar-bottom").toggleClass("navbar-bottom--visible")
+  });
+  var modalButton = $('[data-toggle="modal"]');
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);  
+  closeModalButton.on('click', closeModal);
+
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
 });
