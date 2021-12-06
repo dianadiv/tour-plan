f607b3e1d142da8079ea5752ed4e362b8399aaa4
dianadiv <diana.gs202@gmail.com>
2021-12-06 21:00:42 +0200

    create form validation

--- js/main.js
+++ js/main.js
@@ -26,7 +26,8 @@ $(document).ready(function () {
 
   var menuButton = $(".menu-button");
   menuButton.on('click', function(){
-   $(".navbar-bottom").toggleClass("navbar-bottom--visible")
+   $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+   
   });
   var modalButton = $('[data-toggle="modal"]');
   var closeModalButton = $(".modal__close");
@@ -46,11 +47,39 @@ $(document).ready(function () {
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
   }
-  var modal = $('.modal');
+   
   $(document).keydown(function(event) { 
   if (event.keyCode == 27) { 
-    $('.modal').hide(); 
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
   }
 });
+  //Обработка форм
+  $(".form").each(function() {
+    $(this).validate({
+    errorClass: "invalid",
+    messages: {
+    name: {
+          required: "Please specify your name",
+          minlength: "The name must be at least two characters"
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        phone: {
+          required: "Phone is required",
+        },
+  }
 });
- 
+  })
+
+
+  $(document).ready(function(){
+    $('.phone').mask('+7 (999) 999-99-99');
+  });
+});
+
+         
